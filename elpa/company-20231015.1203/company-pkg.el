bfacb4ed3d975aa5c5f4fb3b59222b24e3e18df0
hixac <darkinbladechadshin92321@gmail.com>
2023-10-29 01:16:26 +0700

    Something new...

---
+++ elpa/company-20231015.1203/company-pkg.el
@@ -0,0 +1,14 @@
+(define-package "company" "20231015.1203" "Modular text completion framework"
+  '((emacs "25.1"))
+  :commit "38d62541107d8d1ebb3164bdd37eb89291d3907c" :authors
+  '(("Nikolaj Schumacher"))
+  :maintainers
+  '(("Dmitry Gutov" . "dmitry@gutov.dev"))
+  :maintainer
+  '("Dmitry Gutov" . "dmitry@gutov.dev")
+  :keywords
+  '("abbrev" "convenience" "matching")
+  :url "http://company-mode.github.io/")
+;; Local Variables:
+;; no-byte-compile: t
+;; End:
